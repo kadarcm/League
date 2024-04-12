7b9defda6e3cac91506f21caf4871f4a154e9d2f
ckadar <kadarcm@gmail.com>
2024-04-12 19:46:49 -0400

    Team detail page mostly completed

--- Pages/Teams/Details.cshtml.cs
+++ Pages/Teams/Details.cshtml.cs
@@ -1,7 +1,11 @@
 using League.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 using System.Diagnostics;
+using League.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace League.Pages.Teams
 {
@@ -13,6 +17,8 @@ namespace League.Pages.Teams
         //Propeties
         [BindProperty(SupportsGet =true)]
         public string Team {  get; set; }
+        public Team Teamobj { get; set; }
+        public List<Player> Players { get; set; }
 
         //Constructor
         public DetailsModel(LeagueContext leagueDB)
@@ -23,7 +29,16 @@ namespace League.Pages.Teams
         //Page methods
         public void OnGet()
         {
-            Debug.WriteLine(this.Team);
+            var players = from p in _leagueDB.Players
+                          where p.TeamId == this.Team
+                          orderby p.Position, p.Number
+                          select p;
+            Players =players.ToList();
+
+            var tname = from t in _leagueDB.Teams
+                        where t.TeamId == this.Team
+                        select t;
+            this.Teamobj = tname.FirstOrDefault();
         }
     }
 }
