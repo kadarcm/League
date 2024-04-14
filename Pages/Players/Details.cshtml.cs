ad1a453720c62e722bdad423cf1fc65776cdec41
ckadar <kadarcm@gmail.com>
2024-04-14 17:29:27 -0400

    Players Bootstrap added

--- Pages/Players/Details.cshtml.cs
+++ Pages/Players/Details.cshtml.cs
@@ -1,12 +1,28 @@
+using League.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using League.Data;
+using System.Linq;
 
 namespace League.Pages.Players
 {
     public class DetailsModel : PageModel
     {
+        private readonly LeagueContext _leagueDB;
+        public Player Player { get; set; }
+
+        public DetailsModel(LeagueContext leagueDb)
+        {
+            {
+                _leagueDB = leagueDb;
+            }
+        }
         public void OnGet(string ID)
         {
+            var qry = from p in _leagueDB.Players
+                where p.PlayerId == ID
+                select p;
+            this.Player = qry.FirstOrDefault();
         }
     }
 }
